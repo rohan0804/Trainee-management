5ad80c0a019d363b43b43467dc9039e5a08e86d0
nishitarora2200 <nishit.arora2200@gmail.com>
2020-03-16 15:37:47 +0530

    changes in routes and controllers of admin adn auth

--- Controllers/admin.js
+++ Controllers/admin.js
@@ -1,5 +1,8 @@
 const Role = require('../Models/role');
-exports.postAddRole = async (req,res)=>{
+const Department=require('../Models/department');
+const Mentor=require('../Models/mentor');
+
+exports.postAddRole = async (req,res,next)=>{
     const {name} = req.body;
     const roleStatus = await Role.create({
         name
@@ -18,4 +21,31 @@ exports.postAddDepartment=async (req,res,next)=>{
         console.log(departmentDetails);
 };
 
+exports.getTraineeSignup = (req,res)=>{
+    res.render('trainee-signup');
+}
+exports.postTraineeSignup  = async(req,res)=>{
+    const {name,email,password,phone_no,joining_date,last_date} = req.body;
+    console.log(req.body);
+    // const role = await Role.findOne({where:{name:"trainee"}})
+    // const authDetails =await Auth.create({
+    //     email,password,role_id:role.id
+    // });
 
+    // Trainee.create({
+
+    // })
+};
+
+exports.getAddMentor=async (req,res,next)=>{
+    res.render('mentor-signup');
+};
+
+exports.postAddMentor=async (req,res)=>{
+    const {name,email,phoneNo}=req.body;
+    const department = await Department.findOne({where:{name:name}});
+    const mentorDetails=await Mentor.create({
+        name,email,phoneNo,department_id:department.dataValues.id
+    });
+    console.log(mentorDetails);
+};
--- Routes/auth.js
+++ Routes/auth.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getLogin, postTraineeSignup, getTraineeSignup,getAddMentor,postAddMentor} = require('../Controllers/auth');
+const {getLogin} = require('../Controllers/auth');
 router.get('/login', getLogin);
-router.post('/add/trainee', postTraineeSignup);
-router.get('/add/trainee', getTraineeSignup);
-router.post('/add/mentor',postAddMentor);
-router.get('/add/mentor',getAddMentor);
+
 module.exports = router;
