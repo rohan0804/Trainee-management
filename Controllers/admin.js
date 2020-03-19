7874e6486d4dfde5e2ba433e663fd3d05742ca26
shyamalzapbuild@gmail.com <shyamalsharma.zapbuild@gmail.com>
2020-03-19 23:13:06 +0530

    CRUD mentor

--- Controllers/admin.js
+++ Controllers/admin.js
@@ -2,8 +2,9 @@ const Auth = require('../Models/auth');
 const Role = require('../Models/role');
 const Department = require('../Models/department');
 const Mentor = require('../Models/mentor');
-const Auth = require('../Models/auth');
 
+
+//Role
 exports.postAddRole = async (req, res, next) => {
     const { name } = req.body;
     const roleStatus = await Role.create({
@@ -12,21 +13,26 @@ exports.postAddRole = async (req, res, next) => {
     console.log(roleStatus);
 
 }
+
+//Department
 exports.getAddDepartment = async (req, res, next) => {
     res.render('department');
 };
 
 exports.postAddDepartment = async (req, res, next) => {
-    const { name, head, syllabus } = req.body;
+    const { name, head, syllabus,department_head } = req.body;
     const department = await Department.create({
         name:name,
         department_head : head,
-        syllabus:syllabus
+        syllabus:syllabus,
+        department_head:department_head
         
      });
      console.log(department);
+
 };
 
+//Trainee
 exports.getTraineeSignup = async(req, res) => {
     const data = await Department.findAll();
     res.render('trainee-signup',{
@@ -39,13 +45,14 @@ exports.postTraineeSignup = async (req, res) => {
     
 };
 
+//Mentor
 exports.getAddMentor = async (req, res, next) => {
     const departments = await Department.findAll();
     res.render('mentor-signup',{departments:departments});
     
 };
 
-exports.postAddMentor = async (req, res) => {
+exports.postAddMentor = async (req, res,next) => {
 
     const { name, email, phoneNo,department_id,password} = req.body;
     console.log(req.body);
@@ -58,4 +65,19 @@ exports.postAddMentor = async (req, res) => {
         name, email, phoneNo, department_id,auth_id:auth.id
     });
     console.log(mentorDetails);
+};
+
+exports.putAddMentor=async (req,res,next)=>{
+    const updateMentor=await Mentor.update(
+        {name:req.body.name,
+        email:req.body.email,
+        phoneNo:req.body.phoneNo,
+        department_id:req.body.department_id},
+        {where:{id:req.params.id}}
+    );
+    console.log(updateMentor);
+};
+
+exports.deleteMentor=async (req,res,next)=>{
+    const deletementor=await Mentor.destroy({where:{id:req.params.id}});
 };
--- Routes/admin.js
+++ Routes/admin.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const {postAddRole,postAddDepartment,getAddDepartment} = require('../Controllers/admin');
-const { postTraineeSignup, getTraineeSignup,getAddMentor,postAddMentor} = require('../Controllers/admin');
+const { postTraineeSignup, getTraineeSignup,getAddMentor,postAddMentor,putAddMentor,deleteMentor} = require('../Controllers/admin');
 router.post('/add/role',postAddRole);
 router.post('/add/department',postAddDepartment);
 router.get('/add/department',getAddDepartment);
 router.post('/add/trainee', postTraineeSignup);
 router.get('/add/trainee', getTraineeSignup);
-router.post('/add/mentor',postAddMentor);
 router.get('/add/mentor',getAddMentor);
+router.post('/add/mentor',postAddMentor);
+router.put('/add/mentor/:id',putAddMentor);
+router.delete('/add/mentor/:id',deleteMentor)
 
 router.get('/add/department',getAddDepartment);
 module.exports = router;
